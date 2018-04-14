2f104aaaae885e8b7fa3833dd5a85a1408931d9e
Zhuanglijuan <zhuangljwork@163.com>
2018-04-14 22:40:18 +0800

    SpringMVC configuration development

---
+++ 02-simpleUrlHandlerMapping/src/main/java/com/jxufe/handlers/MyController.java
@@ -0,0 +1,22 @@
+package com.jxufe.handlers;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Zhuang on 2018/4/14.
+ */
+public class MyController implements Controller {
+
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest,
+                                      HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        //其底层执行的是request.setAttribute()
+        mv.addObject("message", "hello SpringMVC World");
+        mv.setViewName("welcome");
+        return mv;
+    }
+}
