bdb56b061df982d4b75ee1b1912fbb4673d5bd0a
Zhuanglijuan <zhuangljwork@163.com>
2018-04-22 16:29:22 +0800

    08-interceptor

---
+++ 08-interceptor/src/main/java/com/jxufe/interceptors/OneInterceptor.java
@@ -0,0 +1,25 @@
+package com.jxufe.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Zhuang on 2018/4/22.
+ */
+public class OneInterceptor implements HandlerInterceptor{
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("执行OneInterceptor ---- preHandle()-----");
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("执行OneInterceptor ---- postHandle()-----");
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("执行OneInterceptor ---- afterCompletion()-----");
+    }
+}
